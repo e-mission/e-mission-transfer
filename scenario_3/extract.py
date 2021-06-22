bf5bc225f6941f81b9f53dde2839581f7ffcfb15
Shankari <shankari@eecs.berkeley.edu>
2021-06-22 16:28:37 -0700

    Fixes from production
    + remember to add `--` to separate the email list from the other arguments
    + remove the channel from the `format` part
    + remove the sample container name (wtf!!)
    + refer to `user_email` correctly

--- scenario_3/extract.py
+++ scenario_3/extract.py
@@ -6,12 +6,13 @@ import os
 import logging
 
 def run(command):
+    print(f"Running {command}")
     os.system(command)
 
 def extract_and_copy(user_email, container):
-    run(f"docker exec {container} bash -c 'source setup/activate.sh; ./e-mission-py.bash bin/debug/extract_timeline_for_day_range_and_user.py -e {user_email} 2020-01-01 2025-01-01 /tmp/emission_ind'")
+    run(f"docker exec {container} bash -c 'cd e-mission-server; source setup/activate.sh; ./e-mission-py.bash bin/debug/extract_timeline_for_day_range_and_user.py -e {user_email} -- 2020-01-01 2025-01-01 /tmp/emission_ind'")
     run(f"docker exec {container} bash -c 'tar -czvf /tmp/{user_email}.tar.gz /tmp/emission_ind*'")
-    run("docker cp {container}:/tmp/{user_email}.tar.gz /tmp/".format(container=container, channel=channel))
+    run(f"docker cp {container}:/tmp/{user_email}.tar.gz /tmp/")
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
@@ -22,9 +23,8 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
-    fc-stack_fc-analysis-server
     if not args.container:
-        program = user_email.split("_")[0]
+        program = args.user_email.split("_")[0]
         args.container = f"{program}-stack_{program}-analysis-server_1"
         print(f"auto-detecting container as {args.container}")
     extract_and_copy(args.user_email, args.container)
