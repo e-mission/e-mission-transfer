b4571a9762ef35f93875693a9250c3c078211249
Shankari <shankari@eecs.berkeley.edu>
2021-06-24 11:09:30 -0700

    Fixing based on real-time usage
    Specially if we want to invoke this remotely over ssh, we don't want to deal
    with obscure naming conventions for the dump file. So we now require the
    program and allow the container to be overridden, similar to the other script.

--- scenario_3/mongodump.py
+++ scenario_3/mongodump.py
@@ -13,17 +13,17 @@ def run(command):
     print(f"Running {command}")
     os.system(command)
 
-def extract_and_copy(container):
-    dumpname = container + "_"+ dt.today().isoformat()
-    run(f"docker exec {container} bash -c 'cd /tmp; mongodump; tar czvf {dumpname}.tar.gz dump")
+def extract_and_copy(program, container):
+    dumpname = program + "_"+ dt.date.today().isoformat()
+    run(f"docker exec {container} bash -c 'cd /tmp; mongodump'")
+    run(f"docker exec {container} bash -c 'cd /tmp; tar czvf {dumpname}.tar.gz dump'")
     run(f"docker cp {container}:/tmp/{dumpname}.tar.gz /tmp/")
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser(prog="mongodump.py")
 
-    group = parser.add_mutually_exclusive_group(required=True)
-    parser.add_argument("--program", help="the program to download data for")
+    parser.add_argument("program", help="the program to download data for")
     parser.add_argument("--container", help="specify the container instead of inferring from the program")
     
     args = parser.parse_args()
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     if not args.container:
         args.container = f"{args.program}-stack_{args.program}-db_1"
         print(f"auto-detecting container as {args.container}")
-    extract_and_copy(args.container)
+    extract_and_copy(args.program, args.container)
