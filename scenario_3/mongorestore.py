2f50423877fdcf0e1695e3a85d2d8f86793da643
Shankari <shankari@eecs.berkeley.edu>
2021-06-24 19:56:43 -0700

    Production changes to mongorestore
    The easiest argument would be to specify a filename.
    So we try and infer everything else from it.
    But the filename can be a fully qualified path, which then fails when we try to
    move it into `/tmp` and `untar` it
    
    So we instead do the following:
    - expect a filename
    - convert to basename
    - figure out the program
    - connect to the program container
    - copy the file to `/tmp/{basename}`
    - untar; run

--- scenario_3/mongorestore.py
+++ scenario_3/mongorestore.py
@@ -13,22 +13,24 @@ def run(command):
     print(f"Running {command}")
     os.system(command)
 
-def extract_and_copy(container):
+def extract_and_copy(filename, basename, container):
     dumpname = container + "_upload"
-    run(f"docker cp /tmp/{dumpname}.tar.gz {container}:/tmp/{dumpname}.tar.gz")
-    run(f"docker exec {container} bash -c 'cd /tmp; tar xzvf {dumpname}.tar.gz; mongorestore")
+    run(f"docker cp {filename} {container}:/tmp/{basename}")
+    run(f"docker exec {container} bash -c 'cd /tmp; tar xzvf {basename}; mongorestore'")
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser(prog="mongorestore.py")
 
-    group = parser.add_mutually_exclusive_group(required=True)
-    parser.add_argument("--program", help="the program to upload data into")
+    parser.add_argument("mongodump_file", help="the mongodump to upload")
     parser.add_argument("--container", help="specify the container instead of inferring from the program")
-    
+
     args = parser.parse_args()
+    # if mongodump_file is /home/ubuntu/program_upload.tar.gz, basename is program_upload.tar.gz
+    args.basename = os.path.basename(args.mongodump_file)
+    args.program = args.basename.split("_")[0]
 
     if not args.container:
         args.container = f"{args.program}-stack_{args.program}-db_1"
         print(f"auto-detecting container as {args.container}")
-    extract_and_copy(args.container)
+    extract_and_copy(args.mongodump_file, args.basename, args.container)
